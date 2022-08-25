3c810542106e5c177bbf38861a0e28dcfbd6d7ea
mukul2009 <110176430+mukul2009@users.noreply.github.com>
2022-08-25 18:09:42 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
 var score =0;
 
 var gun,bluebubble,redbubble, bullet, backBoard;
-
+var start,startImg;
 var gunImg,bubbleImg, bulletImg, blastImg, backBoardImg;
 var gameover,gameoverImg ;  
 var redBubbleGroup, redBubbleGroup, bulletGroup;
@@ -9,17 +9,26 @@ var reset,resetImg
 var backgroundImg
 var life =3;
 var score=0;
-var gameState=1
+var gameState=0
+var collidedSound , starts2;
+var entry , entryImg ;
+var up ,down;
 
+var fire , fireImg;
 function preload(){
   gunImg = loadImage("spaceship1.png")
   blastImg = loadImage("blast.png")
-gameoverImg = loadImage("go.png")
+gameoverImg = loadImage("gameOver.png")
   // blastImg = Image("blast.png")
-  resetImg = loadImage("reset.png")
+  resetImg = loadImage("restart.png");
    // blastImg = LoadImage("blast.png")
-    // blastImg = loadImage("blast.jpg")
-
+    // blastImg = loadImage("blast.jpg");
+    entryImg = loadImage  ("entry.png");
+    startImg = loadImage  ("stsrt.png");
+fireImg = loadImage ("fire.png");
+collidedSound = loadSound("sound1.wav");
+down = loadImage("down.jpg")
+up = loadImage("up.jpg")
     backgroundImg = loadImage("spacebacground.jpg");
 
   bulletImg = loadImage("bullet1.png")
@@ -46,20 +55,40 @@ function setup() {
   backBoard= createSprite(20, 200, 100,height);
   backBoard.addImage(backBoardImg)
 backBoard.scale = 1.9
-
+         
 gameover = createSprite(700, 300, 100,height);
 gameover.addImage(gameoverImg)
 gameover.scale = 1.1
 
 
+
+
+start = createSprite(700, 300, 100,height);
+start.addImage(startImg)
+start.scale = 1.1
+
+
+entry = createSprite(800, 200, 100,height);
+entry.addImage(entryImg)
+entry.scale = 0.8
+
   gun= createSprite(100, height/2, 50,50);
   gun.addImage(gunImg)
   gun.scale=0.6
 
+
+
+fire = createSprite(100,height/2+300, 50,50);
+  fire.addImage(fireImg);
+fire.scale=0.2;
+
   
-  reset= createSprite(100, height/2, 50,50);
+  reset= createSprite(700, height/2, 50,50);
   reset.addImage(resetImg)
-  reset.scale=0.6;
+  reset.scale=0.1;
+
+
+  
   
 
   bulletGroup = createGroup();   
@@ -96,22 +125,50 @@ function draw() {
   scoreboard.html("Score: "+score)
   scoreboard.style('color:red'); 
   scoreboard.position(width-200,20)
+if (gameState === 0){
+start.visible = true;
+reset.visible = false;  
+gun.visible = false;
+entry.visible = false;
+backBoard.visible = false;
+fire.visible = false;
 
+
+    gameover.visible = false;
+}
   if(gameState===1){
     gun.y=mouseY
     reset.visible = false;  
 gun.visible = true;
     gameover.visible = false;
+   
 
+    fire.visible = true;
+start.visible = false;
+backBoard.visible = true;
+entry.visible = true
+
+
+
+
+
+
+
+
+bulletGroup.visible = true;
     if (frameCount % 80 === 0) {
       drawblueBubble();
     }
+
+
+
+
 
     if (frameCount % 100 === 0) {
       drawredBubble();
     }
 
-    if(keyDown("space")){
+    if(keyDown("space")||(mousePressedOver(fire))){
       shootBullet();
     }
 
@@ -129,11 +186,13 @@ gun.visible = true;
     if(redBubbleGroup.collide(bulletGroup)){
       handleBubbleCollision(redBubbleGroup);
     }
-
-    
   }
-   if(mousePressedOver(reset)){
+  if(mousePressedOver(start)){
     gameState = 1;
+   
+   }
+   if(mousePressedOver(reset)){
+    gameState = 0;
     score = 0;
     life = 3;
    }
@@ -216,7 +275,7 @@ function handleBubbleCollision(bubbleGroup){
 
      //blast= createSprite(bullet.x+60, bullet.y, 50,50);
   //  image(blastImg)
-
+  collidedSound.play()
 
    blast.scale=0.3
    blast.life=20
@@ -244,8 +303,12 @@ function handleGameover(bubbleGroup){
       reset.visible = true;
       blueBubbleGroup.destroyEach();
       gun.visible = false;
-      
-     
+   bulletGroup.destroyEach();
+      fire.visible = false;
+      entry.visible = false;
+      start.visible = false;
+
+
     }
   
 }
